209e58553a39dd4c6a6a90e76e002282ba5ba4ff
alias-rahil <rahil.kabani.4@gmail.com>
2020-02-04 13:38:29 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # VITWifiLogin
-VIT Wifi Login
+
+Uses python3.
+
+# USAGE
+
+Edit the ```credentials.txt``` file with your correct details.
+
+Run the following command: ```python3 new_requests.py```.
---
+++ new_requests.py
@@ -0,0 +1,17 @@
+import requests
+req_url = "http://phc.prontonetworks.com/cgi-bin/authlogin?URI="
+config = open("credentials.txt")
+credentials = config.read().split()
+username = credentials[1].strip()
+password = credentials[3].strip()
+post_data = {
+    "userId": username,
+    "password": password,
+    "serviceName": "ProntoAuthentication",
+    "Submit22": "Login"
+}
+r = requests.post(req_url, data = post_data)
+if "logged" in r.content:
+    print("Succesfully logged in!")
+else:
+    print("Something went wrong!")
